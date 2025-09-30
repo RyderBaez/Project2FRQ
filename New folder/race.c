21bc1586cac86c20c853817542ac0eba607f7736
Ryder Baez <rbbaez@usc.edu>
2025-09-29 22:57:53 -0700

    init

---
+++ New folder/race.c
@@ -0,0 +1,31 @@
+#include "types.h"
+#include "stat.h"
+int main(){
+    int pid = fork();
+    if(pid < 0){
+        prinf(1, "Error forking first child.\n");
+    }
+    else if (pid == 0){
+        printf(1, "Child 1 Executing\n");
+    }
+    else{
+        pid = fork();
+        if(pid < 0){
+            printf(1, "Error forking second child.\n");
+        }
+        else if (pid == 0){
+        printf(1, "Child 2 Executing\n");
+    }
+    else{
+        printf(1, "Parent Waiting\n");
+        int i;
+        for(i = 0; i < 2; i++){
+            wait();
+        }
+        printf(1, "Children completed\n");
+        printf(1, "Parent Executing\n");
+        printf(1, "Parent exiting.\n");
+    }
+    exit();
+    }
+}
